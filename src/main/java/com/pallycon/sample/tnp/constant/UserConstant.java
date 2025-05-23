dd25933138eea894ab30f0dcb1635c55791ce9e2
tony-doverunner <tony.an@doverunner.com>
2025-05-23 17:09:44 +0900

    feat: TnP 관련 상수 추가
    1. 기능추가
    - 내부 서비스 리전 코드와 AWS 리전 코드를 매핑하는 RegionCode enum 클래스 신규 추가
    - 서비스 연동에 필요한 사용자 및 사이트 관련 상수 정의를 위한 UserConstant 클래스 신규 추가
    
    2. 버그수정
    - 없음
    
    3. 개발환경변경
    - 없음
    
    Jira : FWM-3240

---
+++ src/main/java/com/pallycon/sample/tnp/constant/UserConstant.java
@@ -0,0 +1,11 @@
+package com.pallycon.sample.tnp.constant;
+
+public class UserConstant {
+
+    public static final String SITE_ID = "<Site ID>";
+    public static final String USER_ID = "<User ID>";
+    public static final String SITE_KEY = "<Site Key>";
+    public static final String ACCESS_KEY = "<Access Key>";
+    public static final String STORAGE_ID = "<Storage ID>";
+
+}
